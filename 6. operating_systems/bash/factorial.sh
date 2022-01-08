fbd89751668a09bac19c8a585a7b0dfc9691de2b
Claudio Catalani <50852422+CatalaniCD@users.noreply.github.com>
2022-01-07 22:09:20 -0500

    Create factorial.sh

---
+++ 6. operating_systems/bash/factorial.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+number=1
+while [ $number -le 6 ]          #Outer loop: Iterate from 1 to 6
+do
+        temp=$number
+        factorial=1
+        while [ $temp -ge 1 ]    #Inner loop: Iterate from N to 1
+        do
+                factorial=$(($factorial*$temp))
+                temp=$(($temp-1))
+        done
+        echo $factorial
+        number=$(($number+1))
+done
